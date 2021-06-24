fb6e860dcfc1ac28dc6c82874d4f87f4e1ef6fc9
Peter <rongjun.hu.0716@gmail.com>
2021-06-24 14:22:56 +1000

    Finished Desktop styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/styles.scss";
+import rightImage from "./images/image-header-desktop.jpg";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <div className="card__left">
+          <div className="card__left-content">
+            <h1>
+              Get <span>insights</span> that help your business grow.
+            </h1>
+            <p>
+              {" "}
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </p>
+          </div>
+
+          <div className="card__left-info">
+            <div className="data">
+              <h1>10K+</h1>
+              <p>companies</p>
+            </div>
+            <div className="data">
+              <h1>10K+</h1>
+              <p>companies</p>
+            </div>
+            <div className="data">
+              <h1>10K+</h1>
+              <p>companies</p>
+            </div>
+          </div>
+        </div>
+        <div className="card__right">
+          <img src={rightImage} alt="right side" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
